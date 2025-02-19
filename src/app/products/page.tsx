5aed319557fd41a3380b59ad9a796297f3bef5f5
antoniovita <antoniovitafonseca@gmail.com>
2025-02-19 12:33:40 -0300

    shadcn added and initial components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import {Button} from "@/components/ui/button";
+
+const Products = () => {
+    return ( 
+        <div>
+            <h1>F</h1>
+            <Button>F</Button>
+
+        </div>    
+    );
+}
+ 
+export default Products;
